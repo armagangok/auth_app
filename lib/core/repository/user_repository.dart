bfc168d71b2a92d0a92b2c4b6d341daae47bca6b
Armağan Gök <1armagangok@gmail.com>
2021-12-18 17:53:45 +0300

    enum paramaters lovercase

--- lib/core/repository/user_repository.dart
+++ lib/core/repository/user_repository.dart
@@ -4,17 +4,17 @@ import '../services/firebase/services/auth_base.dart';
 import '../services/firebase/services/dummy_service.dart';
 import '../services/firebase/services/services.dart';
 
-enum AppMode { DEBUG, RELEASE }
+enum AppMode { debug, release }
 
 class UserRepository implements AuthBase {
   final FirebaseAuthService _authService = locator<FirebaseAuthService>();
   final DummyService _dummyAuthService = locator<DummyService>();
 
-  AppMode appMode = AppMode.DEBUG;
+  AppMode appMode = AppMode.debug;
 
   @override
   RenewedUser? currentUser() {
-    if (appMode == AppMode.DEBUG) {
+    if (appMode == AppMode.debug) {
       return _dummyAuthService.currentUser();
     } else {
       return _authService.currentUser();
@@ -23,7 +23,7 @@ class UserRepository implements AuthBase {
 
   @override
   Future<bool> signOut() async {
-    if (appMode == AppMode.DEBUG) {
+    if (appMode == AppMode.debug) {
       return await _dummyAuthService.signOut();
     } else {
       return _authService.signOut();
@@ -32,7 +32,7 @@ class UserRepository implements AuthBase {
 
   @override
   Future<RenewedUser?> signinAnonim() async {
-    if (appMode == AppMode.DEBUG) {
+    if (appMode == AppMode.debug) {
       await _dummyAuthService.signinAnonim();
     } else {
       return _authService.signinAnonim();
